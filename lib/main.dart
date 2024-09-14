00879794e73ab7b99ebcf914387ae76c31d2eee8
sothulthorn <thornsothul@gmail.com>
2024-09-14 17:23:57 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:coffee_card_app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Sandbox"),
+          backgroundColor: Colors.grey,
+        ),
+        body: const Row(
+          children: [],
+        ));
+  }
+}
